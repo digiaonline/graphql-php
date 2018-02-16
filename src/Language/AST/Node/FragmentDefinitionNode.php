5813c6e37a7b79e99e7c2b1d9c91e0ff21ca9286
Hung Neo <hungnq1989@gmail.com>
2018-02-16 06:49:35 +0200

    Add FragmentDefinitionNode

---
+++ src/Language/AST/Node/FragmentDefinitionNode.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Digia\GraphQL\Language\AST\Node;
+
+use Digia\GraphQL\Language\AST\ASTNodeInterface;
+use Digia\GraphQL\Language\AST\KindEnum;
+
+/**
+ * Class FragmentDefinitionNode
+ * @package Digia\GraphQL\Language\AST\Node
+ */
+class FragmentDefinitionNode implements ASTNodeInterface
+{
+
+    /**
+     * @return string
+     */
+    public function getKind(): string
+    {
+        return KindEnum::FRAGMENT_DEFINITION;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        // TODO: Implement getValue() method.
+    }
+}
