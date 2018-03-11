d3e8851bb501c8218a714233df5358aac90b9a9c
Christoffer Niska <ChristofferNiska@gmail.com>
2018-03-12 01:40:48 +0200

    Public API (#72)
    * Add proper public API
    
    * Add execute to public API

--- src/api.php
+++ src/api.php
@@ -3,16 +3,16 @@
 namespace Digia\GraphQL;
 
 use Digia\GraphQL\Error\InvariantException;
+use Digia\GraphQL\Error\SyntaxErrorException;
 use Digia\GraphQL\Error\ValidationException;
-use Digia\GraphQL\Execution\Execution;
 use Digia\GraphQL\Execution\ExecutionInterface;
 use Digia\GraphQL\Execution\ExecutionResult;
+use Digia\GraphQL\Language\LexerInterface;
 use Digia\GraphQL\Language\Node\DocumentNode;
 use Digia\GraphQL\Language\Node\NodeInterface;
-use Digia\GraphQL\Language\SchemaBuilder\SchemaBuilderInterface;
-use Digia\GraphQL\Language\LexerInterface;
 use Digia\GraphQL\Language\ParserInterface;
 use Digia\GraphQL\Language\PrinterInterface;
+use Digia\GraphQL\Language\SchemaBuilder\SchemaBuilderInterface;
 use Digia\GraphQL\Language\Source;
 use Digia\GraphQL\Type\SchemaInterface;
 use Digia\GraphQL\Util\SerializationInterface;
@@ -23,6 +23,7 @@ use Digia\GraphQL\Validation\ValidatorInterface;
  * @param array         $options
  * @return NodeInterface|DocumentNode|SerializationInterface
  * @throws InvariantException
+ * @throws SyntaxErrorException
  */
 function parse($source, array $options = []): NodeInterface
 {
@@ -38,6 +39,7 @@ function parse($source, array $options = []): NodeInterface
  * @param array         $options
  * @return NodeInterface|SerializationInterface
  * @throws InvariantException
+ * @throws SyntaxErrorException
  */
 function parseValue($source, array $options = []): NodeInterface
 {
@@ -53,6 +55,7 @@ function parseValue($source, array $options = []): NodeInterface
  * @param array         $options
  * @return NodeInterface|SerializationInterface
  * @throws InvariantException
+ * @throws SyntaxErrorException
  */
 function parseType($source, array $options = []): NodeInterface
 {
@@ -75,15 +78,6 @@ function buildSchema(string $source, array $options = []): SchemaInterface
 }
 
 /**
- * @param NodeInterface $node
- * @return string
- */
-function printNode(NodeInterface $node): string
-{
-    return GraphQL::get(PrinterInterface::class)->print($node);
-}
-
-/**
  * @param SchemaInterface $schema
  * @param DocumentNode    $document
  * @return array|ValidationException[]
@@ -95,11 +89,51 @@ function validate(SchemaInterface $schema, DocumentNode $document): array
 
 /**
  * @param SchemaInterface $schema
+ * @param DocumentNode    $document
+ * @param mixed|null      $rootValue
+ * @param mixed|null      $contextValue
+ * @param array           $variableValues
+ * @param mixed|null      $operationName
+ * @param callable|null   $fieldResolver
+ * @return ExecutionResult
+ */
+function execute(
+    SchemaInterface $schema,
+    DocumentNode $document,
+    $rootValue = null,
+    $contextValue = null,
+    array $variableValues = [],
+    $operationName = null,
+    callable $fieldResolver = null
+): ExecutionResult {
+    return GraphQL::get(ExecutionInterface::class)
+        ->execute(
+            $schema,
+            $document,
+            $rootValue,
+            $contextValue,
+            $variableValues,
+            $operationName,
+            $fieldResolver
+        );
+}
+
+/**
+ * @param NodeInterface $node
+ * @return string
+ */
+function printNode(NodeInterface $node): string
+{
+    return GraphQL::get(PrinterInterface::class)->print($node);
+}
+
+/**
+ * @param SchemaInterface $schema
  * @param string          $source
- * @param null            $rootValue
- * @param null            $contextValue
- * @param null            $variableValues
- * @param null            $operationName
+ * @param mixed|null      $rootValue
+ * @param mixed|null      $contextValue
+ * @param array           $variableValues
+ * @param mixed|null      $operationName
  * @param callable|null   $fieldResolver
  * @return ExecutionResult
  * @throws InvariantException
@@ -109,19 +143,32 @@ function graphql(
     string $source,
     $rootValue = null,
     $contextValue = null,
-    $variableValues = [],
+    array $variableValues = [],
     $operationName = null,
     callable $fieldResolver = null
 ): ExecutionResult {
-    /** @noinspection PhpParamsInspection */
-    return GraphQL::get(ExecutionInterface::class)
-        ->execute(
-            $schema,
-            parse($source),
-            $rootValue,
-            $contextValue,
-            $variableValues,
-            $operationName,
-            $fieldResolver
-        );
+    // TODO: Validate schema
+
+    $document = null;
+
+    try {
+        $document = parse($source);
+    } catch (SyntaxErrorException $error) {
+        return new ExecutionResult([], [$error]);
+    }
+
+    $validationErrors = validate($schema, $document);
+    if (!empty($validationErrors)) {
+        return new ExecutionResult([], $validationErrors);
+    }
+
+    return execute(
+        $schema,
+        $document,
+        $rootValue,
+        $contextValue,
+        $variableValues,
+        $operationName,
+        $fieldResolver
+    );
 }
