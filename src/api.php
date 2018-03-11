f374af0f7c3168a884d84ab43f12559c311d3c19
Christoffer Niska <christofferniska@gmail.com>
2018-03-11 23:39:51 +0200

    Add execute to public API

--- src/api.php
+++ src/api.php
@@ -5,15 +5,14 @@ namespace Digia\GraphQL;
 use Digia\GraphQL\Error\InvariantException;
 use Digia\GraphQL\Error\SyntaxErrorException;
 use Digia\GraphQL\Error\ValidationException;
-use Digia\GraphQL\Execution\Execution;
 use Digia\GraphQL\Execution\ExecutionInterface;
 use Digia\GraphQL\Execution\ExecutionResult;
+use Digia\GraphQL\Language\LexerInterface;
 use Digia\GraphQL\Language\Node\DocumentNode;
 use Digia\GraphQL\Language\Node\NodeInterface;
-use Digia\GraphQL\Language\SchemaBuilder\SchemaBuilderInterface;
-use Digia\GraphQL\Language\LexerInterface;
 use Digia\GraphQL\Language\ParserInterface;
 use Digia\GraphQL\Language\PrinterInterface;
+use Digia\GraphQL\Language\SchemaBuilder\SchemaBuilderInterface;
 use Digia\GraphQL\Language\Source;
 use Digia\GraphQL\Type\SchemaInterface;
 use Digia\GraphQL\Util\SerializationInterface;
@@ -24,6 +23,7 @@ use Digia\GraphQL\Validation\ValidatorInterface;
  * @param array         $options
  * @return NodeInterface|DocumentNode|SerializationInterface
  * @throws InvariantException
+ * @throws SyntaxErrorException
  */
 function parse($source, array $options = []): NodeInterface
 {
@@ -39,6 +39,7 @@ function parse($source, array $options = []): NodeInterface
  * @param array         $options
  * @return NodeInterface|SerializationInterface
  * @throws InvariantException
+ * @throws SyntaxErrorException
  */
 function parseValue($source, array $options = []): NodeInterface
 {
@@ -54,6 +55,7 @@ function parseValue($source, array $options = []): NodeInterface
  * @param array         $options
  * @return NodeInterface|SerializationInterface
  * @throws InvariantException
+ * @throws SyntaxErrorException
  */
 function parseType($source, array $options = []): NodeInterface
 {
@@ -86,6 +88,37 @@ function validate(SchemaInterface $schema, DocumentNode $document): array
 }
 
 /**
+ * @param SchemaInterface $schema
+ * @param DocumentNode    $document
+ * @param mixed|null      $rootValue
+ * @param mixed|null      $contextValue
+ * @param array           $variableValues
+ * @param mixed|null      $operationName
+ * @param callable|null   $fieldResolver
+ * @return ExecutionResult
+ */
+function execute(
+    SchemaInterface $schema,
+    DocumentNode $document,
+    $rootValue = null,
+    $contextValue = null,
+    array $variableValues = [],
+    $operationName = null,
+    callable $fieldResolver = null
+): ExecutionResult {
+    return GraphQL::get(ExecutionInterface::class)
+        ->execute(
+            $schema,
+            $document,
+            $rootValue,
+            $contextValue,
+            $variableValues,
+            $operationName,
+            $fieldResolver
+        );
+}
+
+/**
  * @param NodeInterface $node
  * @return string
  */
@@ -97,10 +130,10 @@ function printNode(NodeInterface $node): string
 /**
  * @param SchemaInterface $schema
  * @param string          $source
- * @param null            $rootValue
- * @param null            $contextValue
- * @param null            $variableValues
- * @param null            $operationName
+ * @param mixed|null      $rootValue
+ * @param mixed|null      $contextValue
+ * @param array           $variableValues
+ * @param mixed|null      $operationName
  * @param callable|null   $fieldResolver
  * @return ExecutionResult
  * @throws InvariantException
@@ -110,7 +143,7 @@ function graphql(
     string $source,
     $rootValue = null,
     $contextValue = null,
-    $variableValues = [],
+    array $variableValues = [],
     $operationName = null,
     callable $fieldResolver = null
 ): ExecutionResult {
@@ -129,15 +162,13 @@ function graphql(
         return new ExecutionResult([], $validationErrors);
     }
 
-    /** @noinspection PhpParamsInspection */
-    return GraphQL::get(ExecutionInterface::class)
-        ->execute(
-            $schema,
-            parse($source),
-            $rootValue,
-            $contextValue,
-            $variableValues,
-            $operationName,
-            $fieldResolver
-        );
+    return execute(
+        $schema,
+        $document,
+        $rootValue,
+        $contextValue,
+        $variableValues,
+        $operationName,
+        $fieldResolver
+    );
 }
