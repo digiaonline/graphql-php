403be5ab8c5f3ff9e337bab3e78b4fa643329d46
Christoffer Niska <ChristofferNiska@gmail.com>
2018-03-12 16:41:58 +0200

    Add FragmentsOnCompositeTypesRule (#57)

--- src/Provider/RulesProvider.php
+++ src/Provider/RulesProvider.php
@@ -4,6 +4,7 @@ namespace Digia\GraphQL\Provider;
 
 use Digia\GraphQL\Validation\Rule\ExecutableDefinitionsRule;
 use Digia\GraphQL\Validation\Rule\FieldOnCorrectTypeRule;
+use Digia\GraphQL\Validation\Rule\FragmentsOnCompositeTypesRule;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class RulesProvider extends AbstractServiceProvider
@@ -14,6 +15,7 @@ class RulesProvider extends AbstractServiceProvider
     protected $provides = [
         ExecutableDefinitionsRule::class,
         FieldOnCorrectTypeRule::class,
+        FragmentsOnCompositeTypesRule::class,
     ];
 
     /**
