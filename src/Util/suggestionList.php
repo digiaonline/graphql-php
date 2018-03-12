fb02019fdadca7737feda6bf4803895aa254b160
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-03-12 16:21:29 +0200

    Use levenshtein() instead of homegrown lexicalDistance()

--- src/Util/suggestionList.php
+++ src/Util/suggestionList.php
@@ -15,7 +15,7 @@ function suggestionList(string $input, array $options): array
 
     /** @noinspection ForeachInvariantsInspection */
     for ($i = 0; $i < $oLength; $i++) {
-        $distance = lexicalDistance($input, $options[$i]);
+        $distance = levenshtein($input, $options[$i]);
         $threshold = max($inputThreshold, strlen($options[$i]) / 2, 1);
         if ($distance <= $threshold) {
             $optionsByDistance[$options[$i]] = $distance;
@@ -29,50 +29,4 @@ function suggestionList(string $input, array $options): array
     });
 
     return $result;
-}
-
-/**
- * @param string $aStr
- * @param string $bStr
- * @return int
- */
-function lexicalDistance(string $aStr, string $bStr): int
-{
-    if ($aStr === $bStr) {
-        return 0;
-    }
-
-    $d = [];
-    $a = strtolower($aStr);
-    $b = strtolower($bStr);
-    $aLength = strlen($a);
-    $bLength = strlen($b);
-
-    if ($a === $b) {
-        return 1;
-    }
-
-    /** @noinspection ForeachInvariantsInspection */
-    for ($i = 0; $i <= $aLength; $i++) {
-        $d[$i] = [$i];
-    }
-
-    /** @noinspection ForeachInvariantsInspection */
-    for ($j = 1; $j <= $bLength; $j++) {
-        $d[0][$j] = $i;
-    }
-
-    for ($i = 1; $i <= $aLength; $i++) {
-        for ($j = 1; $j <= $bLength; $j++) {
-            $cost = $a[$i - 1] === $b[$j - 1] ? 0 : 1;
-
-            $d[$i][$j] = min($d[$i - 1][$j] + 1, $d[$i][$j - 1] + 1, $d[$i - 1][$j - 1] + $cost);
-
-            if ($i > 1 && $j > 1 && $a[$i - 1] === $b[$j - 2] && $a[$i - 2] === $b[$j - 1]) {
-                $d[$i][$j] = min($d[$i][$j], $d[$i - 2][$j - 2] + $cost);
-            }
-        }
-    }
-
-    return $d[$aLength][$bLength];
 }
