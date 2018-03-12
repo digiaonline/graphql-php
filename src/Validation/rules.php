3ec7ebb8b7d2fce037ef89a4dccb4e989f9ab181
Christoffer Niska <ChristofferNiska@gmail.com>
2018-03-12 21:28:09 +0200

    Add KnownTypeNamesRule (#63)

--- src/Validation/rules.php
+++ src/Validation/rules.php
@@ -9,6 +9,7 @@ use Digia\GraphQL\Validation\Rule\FragmentsOnCompositeTypesRule;
 use Digia\GraphQL\Validation\Rule\KnownArgumentNamesRule;
 use Digia\GraphQL\Validation\Rule\KnownDirectivesRule;
 use Digia\GraphQL\Validation\Rule\KnownFragmentNamesRule;
+use Digia\GraphQL\Validation\Rule\KnownTypeNamesRule;
 use Digia\GraphQL\Validation\Rule\PossibleFragmentSpreadsRule;
 
 /**
@@ -23,6 +24,7 @@ function specifiedRules(): array
         GraphQL::get(KnownArgumentNamesRule::class),
         GraphQL::get(KnownDirectivesRule::class),
         GraphQL::get(KnownFragmentNamesRule::class),
+        GraphQL::get(KnownTypeNamesRule::class),
         GraphQL::get(PossibleFragmentSpreadsRule::class),
     ];
 }
