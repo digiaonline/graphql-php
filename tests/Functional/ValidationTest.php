5040599d1474c45cfb4f4f90bf0a04f96dac157b
Christoffer Niska <ChristofferNiska@gmail.com>
2018-03-20 02:03:38 +0200

    Add validation tests (#140)

---
+++ tests/Functional/ValidationTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Digia\GraphQL\Test\Functional;
+
+use Digia\GraphQL\Language\Source;
+use Digia\GraphQL\Test\TestCase;
+use function Digia\GraphQL\parse;
+use function Digia\GraphQL\validate;
+
+class ValidationTest extends TestCase
+{
+    // Star Wars Validation Tests
+
+    // Basic Queries
+
+    // Validates a complex but valid query
+
+    public function testValidatesAComplexButValidQuery()
+    {
+        $query = '
+        query NestedQueryWithFragment {
+          hero {
+            ...NameAndAppearances
+            friends {
+              ...NameAndAppearances
+              friends {
+                ...NameAndAppearances
+              }
+            }
+          }
+        }
+        
+        fragment NameAndAppearances on Character {
+          name
+          appearsIn
+        }
+        ';
+
+        $this->assertEmpty($this->validateQuery($query));
+    }
+
+    // Notes that non-existent fields are invalid
+
+    public function testNodesThatNonExistentFieldsAreInvalid()
+    {
+        $query = '
+        query HeroSpaceshipQuery {
+          hero {
+            favoriteSpaceship
+          }
+        }
+        ';
+
+        $this->assertNotEmpty($this->validateQuery($query));
+    }
+
+    // Requires fields on objects
+
+    public function testRequiresFieldsOnObjects()
+    {
+        $query = '
+        query HeroNoFieldsQuery {
+          hero
+        }
+        ';
+
+        $this->assertNotEmpty($this->validateQuery($query));
+    }
+
+    // Disallows fields on scalars
+
+    public function testDisallowsFieldsOnScalars()
+    {
+        $query = '
+        query HeroNoFieldsQuery {
+          hero {
+            name {
+              firstCharacterOfName
+            }
+          }
+        }
+        ';
+
+        $this->assertNotEmpty($this->validateQuery($query));
+    }
+
+    // Disallows object fields on interfaces
+
+    public function testDisallowsObjectFieldsOnInterfaces()
+    {
+        $query = '
+        query DroidFieldOnCharacter {
+          hero {
+            name
+            primaryFunction
+          }
+        }
+        ';
+
+        $this->assertNotEmpty($this->validateQuery($query));
+    }
+
+    // Allows object fields in fragments
+
+    public function testAllowsObjectFieldsInFragments()
+    {
+        $query = '
+        query DroidFieldInFragment {
+          hero {
+            name
+            ...DroidFields
+          }
+        }
+        
+        fragment DroidFields on Droid {
+          primaryFunction
+        }
+        ';
+
+        $this->assertEmpty($this->validateQuery($query));
+    }
+
+    // Allows object fields in inline fragments
+
+    public function testAllowsObjectFieldsInInlineFragments()
+    {
+        $query = '
+        query DroidFieldInFragment {
+          hero {
+            name
+            ... on Droid {
+              primaryFunction
+            }
+          }
+        }
+        ';
+
+        $this->assertEmpty($this->validateQuery($query));
+    }
+
+    protected function validateQuery($query)
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $source = new Source($query, 'StarWars.graphql');
+        /** @noinspection PhpUnhandledExceptionInspection */
+        return validate(starWarsSchema(), parse($source));
+    }
+}
