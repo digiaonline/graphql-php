d2fd99b26f1eb76214cbe2bc46bb1c58e5cd191b
Christoffer Niska <christofferniska@gmail.com>
2018-03-21 17:32:39 +0200

    Add initial support for schema extension (WIP)

---
+++ src/SchemaExtender/SchemaExtenderInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Digia\GraphQL\SchemaExtender;
+
+use Digia\GraphQL\Language\Node\DocumentNode;
+use Digia\GraphQL\Type\SchemaInterface;
+
+interface SchemaExtenderInterface
+{
+    /**
+     * @param SchemaInterface $schema
+     * @param DocumentNode    $document
+     * @param array           $options
+     * @return SchemaInterface
+     */
+    public function extend(SchemaInterface $schema, DocumentNode $document, array $options = []): SchemaInterface;
+}
