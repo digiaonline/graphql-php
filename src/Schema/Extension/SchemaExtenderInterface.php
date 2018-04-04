c4705d413a5b473cc8702ebc527da2caf791ffc5
Christoffer Niska <christofferniska@gmail.com>
2018-04-04 11:41:46 +0300

    Add support for providing a resolver registry when extending a schema

--- src/Schema/Extension/SchemaExtenderInterface.php
+++ src/Schema/Extension/SchemaExtenderInterface.php
@@ -3,21 +3,34 @@
 namespace Digia\GraphQL\Schema\Extension;
 
 use Digia\GraphQL\Language\Node\DocumentNode;
+use Digia\GraphQL\Schema\Resolver\ResolverRegistryInterface;
 use Digia\GraphQL\Schema\SchemaInterface;
 
 interface SchemaExtenderInterface
 {
     /**
-     * @param SchemaInterface $schema
-     * @param DocumentNode    $document
+     * @param SchemaInterface                $schema
+     * @param DocumentNode                   $document
+     * @param ResolverRegistryInterface|null $resolverRegistry
+     * @param array                          $options
      * @return SchemaInterface
      */
-    public function extend(SchemaInterface $schema, DocumentNode $document): SchemaInterface;
+    public function extend(
+        SchemaInterface $schema,
+        DocumentNode $document,
+        ?ResolverRegistryInterface $resolverRegistry = null,
+        array $options = []
+    ): SchemaInterface;
 
     /**
-     * @param SchemaInterface $schema
-     * @param DocumentNode    $document
+     * @param SchemaInterface                $schema
+     * @param DocumentNode                   $document
+     * @param ResolverRegistryInterface|null $resolverRegistry
      * @return ExtensionContextInterface
      */
-    public function createContext(SchemaInterface $schema, DocumentNode $document): ExtensionContextInterface;
+    public function createContext(
+        SchemaInterface $schema,
+        DocumentNode $document,
+        ?ResolverRegistryInterface $resolverRegistry
+    ): ExtensionContextInterface;
 }
