0c163e3ebf2afe29272ee8783d6d8f8344d483fd
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-04-04 19:44:18 +0300

    Fix two bugs in the StringReader, fixes escaped string handling and multi-byte handling
    The JavaScript String.fromCharCode() function doesn't really have a good equivalent in PHP, luckily it's easy to cheat here and achieve the same result

--- src/Language/TokenReader.php
+++ src/Language/TokenReader.php
@@ -278,39 +278,30 @@ class TokenReader implements TokenReaderInterface
       int $col,
       Token $prev
     ): Token {
-        $body = $this->lexer->getBody();
+        $body       = $this->lexer->getBody();
         $bodyLength = mb_strlen($body);
-        $start = $pos;
-        $pos = $start + 1;
+        $start      = $pos;
+        $pos        = $start + 1;
         $chunkStart = $pos;
-        $value = '';
-
-        while ($pos < $bodyLength && ($code = charCodeAt($body,
-            $pos)) !== null && !$this->isLineTerminator($code)) {
+        $value      = '';
+        while ($pos < $bodyLength && ($code = charCodeAt($body, $pos)) !== null && !$this->isLineTerminator($code)) {
             // Closing Quote (")
             if ($code === 34) {
                 $value .= sliceString($body, $chunkStart, $pos);
-
-                return new Token(TokenKindEnum::STRING, $start, $pos + 1, $line,
-                  $col, $prev, $value);
+                return new Token(TokenKindEnum::STRING, $start, $pos + 1, $line, $col, $prev, $value);
             }
-
             if ($this->isSourceCharacter($code)) {
                 throw new SyntaxErrorException(
-                  $this->lexer->getSource(),
-                  $pos,
-                  sprintf('Invalid character within String: %s',
-                    printCharCode($code))
+                    $this->lexer->getSource(),
+                    $pos,
+                    sprintf('Invalid character within String: %s', printCharCode($code))
                 );
             }
-
             ++$pos;
-
             if ($code === 92) {
                 // \
-                $value .= sliceString($body, $chunkStart, $pos + 1);
-                $code = charCodeAt($body, $pos);
-
+                $value .= sliceString($body, $chunkStart, $pos - 1);
+                $code  = charCodeAt($body, $pos);
                 switch ($code) {
                     case 34:
                         $value .= '"';
@@ -338,41 +329,38 @@ class TokenReader implements TokenReaderInterface
                         break;
                     case 117:
                         // u
-                        $charCode = uniCharCode(
-                          charCodeAt($body, $pos + 1),
-                          charCodeAt($body, $pos + 2),
-                          charCodeAt($body, $pos + 3),
-                          charCodeAt($body, $pos + 4)
+                        $unicodeString = sliceString($body, $pos - 1, $pos + 5);
+                        $charCode      = uniCharCode(
+                            charCodeAt($body, $pos + 1),
+                            charCodeAt($body, $pos + 2),
+                            charCodeAt($body, $pos + 3),
+                            charCodeAt($body, $pos + 4)
                         );
                         if ($charCode < 0) {
                             throw new SyntaxErrorException(
-                              $this->lexer->getSource(),
-                              $pos,
-                              sprintf(
-                                'Invalid character escape sequence: \\u%s',
-                                sliceString($body, $pos + 1, $pos + 5)
-                              )
+                                $this->lexer->getSource(),
+                                $pos,
+                                sprintf(
+                                    'Invalid character escape sequence: %s',
+                                    $unicodeString
+                                )
                             );
                         }
-                        $value .= chr($charCode);
-                        $pos += 4;
+                        $value .= $unicodeString;
+                        $pos   += 4;
                         break;
                     default:
                         throw new SyntaxErrorException(
-                          $this->lexer->getSource(),
-                          $pos,
-                          sprintf('Invalid character escape sequence: \\%s',
-                            chr($code))
+                            $this->lexer->getSource(),
+                            $pos,
+                            sprintf('Invalid character escape sequence: \\%s', chr($code))
                         );
                 }
-
                 ++$pos;
                 $chunkStart = $pos;
             }
         }
-
-        throw new SyntaxErrorException($this->lexer->getSource(), $pos,
-          'Unterminated string.');
+        throw new SyntaxErrorException($this->lexer->getSource(), $pos - 1, 'Unterminated string.');
     }
 
     /**
