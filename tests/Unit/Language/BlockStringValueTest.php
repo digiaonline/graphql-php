6cf8a13c3ea34b89a51a10102c12b164cacfe525
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-04-04 11:22:44 +0300

    Add block string value unit tests
    Closes #10

---
+++ tests/Unit/Language/BlockStringValueTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Digia\GraphQL\Test\Unit\Language;
+
+use Digia\GraphQL\Test\TestCase;
+use function Digia\GraphQL\Language\blockStringValue;
+
+/**
+ * Class BlockStringValueTest
+ * @package Digia\GraphQL\Test\Unit\Language
+ */
+class BlockStringValueTest extends TestCase
+{
+
+    public function testRemovesUniformIndentation(): void
+    {
+        $rawStringLines = [
+            '',
+            '    Hello',
+            '      World!',
+            '',
+            '    Yours,',
+            '      GraphQL.'
+        ];
+
+        $expectedBlockStringLines = [
+            'Hello',
+            '  World!',
+            '',
+            'Yours,',
+            '  GraphQL.'
+        ];
+
+        $this->assertBlockStringEquals($expectedBlockStringLines, $rawStringLines);
+    }
+
+    public function testRemovesEmptyLeadingAndTrailingLines(): void
+    {
+        $rawStringLines = [
+            '',
+            '',
+            '    Hello',
+            '      World!',
+            '',
+            '    Yours,',
+            '      GraphQL.',
+            '',
+            '',
+        ];
+
+        $expectedBlockStringLines = [
+            'Hello',
+            '  World!',
+            '',
+            'Yours,',
+            '  GraphQL.'
+        ];
+
+        $this->assertBlockStringEquals($expectedBlockStringLines, $rawStringLines);
+    }
+
+    public function testRemovesBlankLeadingAndTrailingLines(): void
+    {
+        $rawStringLines = [
+            '  ',
+            '        ',
+            '    Hello',
+            '      World!',
+            '',
+            '    Yours,',
+            '      GraphQL.',
+            '        ',
+            '  ',
+        ];
+
+        $expectedBlockStringLines = [
+            'Hello',
+            '  World!',
+            '',
+            'Yours,',
+            '  GraphQL.'
+        ];
+
+        $this->assertBlockStringEquals($expectedBlockStringLines, $rawStringLines);
+    }
+
+    public function testRetainsIndentationFromFirstLine(): void
+    {
+        $rawStringLines = [
+            '    Hello',
+            '      World!',
+            '',
+            '    Yours,',
+            '      GraphQL.',
+            '        ',
+            '  ',
+        ];
+
+        $expectedBlockStringLines = [
+            '    Hello',
+            '  World!',
+            '',
+            'Yours,',
+            '  GraphQL.'
+        ];
+
+        $this->assertBlockStringEquals($expectedBlockStringLines, $rawStringLines);
+    }
+
+    public function testDoesNotAlterTrailingSpaces(): void
+    {
+        $rawStringLines = [
+            '               ',
+            '    Hello,     ',
+            '      World!   ',
+            '               ',
+            '    Yours,     ',
+            '      GraphQL. ',
+            '               ',
+        ];
+
+        $expectedBlockStringLines = [
+            'Hello,     ',
+            '  World!   ',
+            '           ',
+            'Yours,     ',
+            '  GraphQL. ',
+        ];
+
+        $this->assertBlockStringEquals($expectedBlockStringLines, $rawStringLines);
+    }
+
+    /**
+     * @param array $expectedBlockStringLines
+     * @param array $rawStringLines
+     */
+    private function assertBlockStringEquals(array $expectedBlockStringLines, array $rawStringLines): void
+    {
+        $actualBlockString = blockStringValue(implode("\n", $rawStringLines));
+
+        $this->assertEquals(implode("\n", $expectedBlockStringLines), $actualBlockString);
+    }
+}
