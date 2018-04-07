9fc327f72ce7679eeaba315e0134cb451078afd4
Hung Nguyen <hungnq1989@gmail.com>
2018-04-07 13:07:56 +0300

    Test: Default function calls methods

--- tests/Functional/Execution/ResolveTest.php
+++ tests/Functional/Execution/ResolveTest.php
@@ -4,6 +4,7 @@ namespace Digia\GraphQL\Test\Functional\Execution;
 
 use Digia\GraphQL\Test\TestCase;
 use function Digia\GraphQL\graphql;
+use function Digia\GraphQL\Type\Int;
 use function Digia\GraphQL\Type\newObjectType;
 use function Digia\GraphQL\Type\newSchema;
 use function Digia\GraphQL\Type\String;
@@ -41,6 +42,66 @@ class ResolveTest extends TestCase
         $this->assertEquals([
             'data' => [
                 'test' => 'testValue'
+            ]
+        ], $result);
+    }
+
+    /**
+     * Default function calls methods
+     *
+     * @throws \Digia\GraphQL\Error\InvariantException
+     */
+    public function testDefaultFunctionCallsMethods()
+    {
+        $schema = $this->createTestSchema(['type' => String()]);
+
+        $source = [
+            'test' => function () {
+                return 'secretValue';
+            }
+        ];
+
+        $result = graphql($schema, '{ test }', $source);
+
+        $this->assertEquals([
+            'data' => [
+                'test' => 'secretValue'
+            ]
+        ], $result);
+    }
+
+    /**
+     * Default function passes args and context
+     *
+     * @throws \Digia\GraphQL\Error\InvariantException
+     */
+    public function testDefaultFunctionPassesArgsAndContext()
+    {
+        $schema = $this->createTestSchema([
+            'type' => Int(),
+            'args' => ['addend1' => ['type' => Int()]],
+        ]);
+
+        $source = new class(700)
+        {
+            private $num;
+
+            public function __construct($num)
+            {
+                $this->num = $num;
+            }
+
+            public function test($source, $args, $context)
+            {
+                return $this->num + $args['addend1'] + $context['addend2'];
+            }
+        };
+
+        $result = graphql($schema, '{ test(addend1: 80) }', $source, ['addend2' => 9]);
+
+        $this->assertEquals([
+            'data' => [
+                'test' => 789
             ]
         ], $result);
     }
