a41295991ddf1209ccc6b3440087876a401ecad8
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-07-10 12:14:52 +0300

    Update the "unterminated string" test case after rebasing on master
    Now that we fixed the looping, we fixed it so hard that it's impossible to encounter a NUL byte within a string. This changes the loop condition so we can still detect those.

--- src/Language/Lexer.php
+++ src/Language/Lexer.php
@@ -469,8 +469,13 @@ class Lexer implements LexerInterface
         $chunkStart = ++$this->position; // skip the quote
         $value      = '';
 
-        while ($this->position < $this->bodyLength &&
-            ($code = $this->readCharCode($this->position)) !== 0 && !isLineTerminator($code)) {
+        while ($this->position < $this->bodyLength) {
+            $code = $this->readCharCode($this->position);
+
+            if (isLineTerminator($code)) {
+                break;
+            }
+
             // Closing Quote (")
             if (34 === $code) {
                 $value .= sliceString($this->body, $chunkStart, $this->position);
@@ -561,7 +566,9 @@ class Lexer implements LexerInterface
         $chunkStart     = $this->position;
         $rawValue       = '';
 
-        while ($this->position < $this->bodyLength && ($code = $this->readCharCode($this->position)) !== 0) {
+        while ($this->position < $this->bodyLength) {
+            $code = $this->readCharCode($this->position);
+
             // Closing Triple-Quote (""")
             if ($this->isTripleQuote($code)) {
                 $rawValue .= sliceString($this->body, $chunkStart, $this->position);
