55a1fcfb6b6253e4be1395f73840a66cd007510a
Christoffer Niska <christofferniska@gmail.com>
2018-10-03 09:16:35 +0300

    Add unit test for error handler

---
+++ tests/Unit/Error/ErrorHandlerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Digia\GraphQL\Test\Unit\Error;
+
+use Digia\GraphQL\Error\ErrorHandler;
+use Digia\GraphQL\Error\ExecutionException;
+use Digia\GraphQL\Test\TestCase;
+
+class ErrorHandlerTest extends TestCase
+{
+    public function testHandleError()
+    {
+        $wasCallbackInvoked = false;
+
+        $handleCallback = function (ExecutionException $exception) use (&$wasCallbackInvoked) {
+            $wasCallbackInvoked = true;
+        };
+
+        $errorHandler = new ErrorHandler($handleCallback);
+
+        $errorHandler->handleError(new ExecutionException('This is an exception.'));
+
+        $this->assertTrue($wasCallbackInvoked);
+    }
+}
