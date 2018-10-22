9a5d6a31ec7e3469b559fe8dbc17a1e94964d986
Sam Stenvall <sam.stenvall@nordsoftware.com>
2018-10-22 14:50:23 +0300

    Add a broken test case for dealing with false non-null booleans in input types

--- tests/Functional/Execution/ValuesHelperTest.php
+++ tests/Functional/Execution/ValuesHelperTest.php
@@ -3,13 +3,16 @@
 namespace Digia\GraphQL\Test\Functional\Execution;
 
 use Digia\GraphQL\Execution\ExecutionContext;
+use Digia\GraphQL\Execution\ValuesHelper;
 use Digia\GraphQL\Language\Node\ArgumentsAwareInterface;
 use Digia\GraphQL\Language\Node\OperationDefinitionNode;
+use Digia\GraphQL\Language\Node\StringValueNode;
 use Digia\GraphQL\Test\TestCase;
 use function Digia\GraphQL\Execution\coerceArgumentValues;
 use function Digia\GraphQL\Execution\coerceVariableValues;
 use function Digia\GraphQL\parse;
 use function Digia\GraphQL\Type\booleanType;
+use function Digia\GraphQL\Type\newInputObjectType;
 use function Digia\GraphQL\Type\newNonNull;
 use function Digia\GraphQL\Type\newObjectType;
 use function Digia\GraphQL\Type\newSchema;
@@ -96,5 +99,132 @@ class ValuesHelperTest extends TestCase
         $coercedValue = coerceVariableValues($schema, $variableDefinitions, ['shout' => null]);
         $this->assertEquals([], $coercedValue->getValue());
         $this->assertTrue($coercedValue->hasErrors());
+    }
+
+    public function testCoerceValuesForInputObjectTypes(): void
+    {
+        // Test input object types
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $schema = newSchema([
+            'query' => newObjectType([
+                'name'   => 'Query',
+                'fields' => [
+                    'inputObjectField' => [
+                        'type' => booleanType(),
+                        'args' => [
+                            'inputObject' => [
+                                'type' => newInputObjectType([
+                                        'name'   => 'InputObject',
+                                        'fields' => [
+                                            'a' => ['type' => stringType()],
+                                            'b' => ['type' => newNonNull(stringType())]
+                                        ]
+                                    ]
+                                )
+                            ],
+                        ],
+                    ],
+                ],
+            ]),
+        ]);
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $documentNode = parse('
+            query ($inputObject: InputObject!) {
+                inputObjectField(inputObject: $inputObject)
+            }
+         ');
+
+        /** @var OperationDefinitionNode $operation */
+        $operation           = $documentNode->getDefinitions()[0];
+        $variableDefinitions = $operation->getVariableDefinitions();
+
+        // Test with a missing non-null string
+        $coercedValue = coerceVariableValues($schema, $variableDefinitions, [
+            'inputObject' => [
+                'a' => 'some string'
+            ]
+        ]);
+
+        $this->assertTrue($coercedValue->hasErrors());
+        $this->assertEquals('Variable "$inputObject" got invalid value {"a":"some string"}; Field value.b of required type String! was not provided.',
+            $coercedValue->getErrors()[0]->getMessage());
+
+        // Test again with all variables, no errors expected
+        $coercedValue = coerceVariableValues($schema, $variableDefinitions, [
+            'inputObject' => [
+                'a' => 'some string',
+                'b' => 'some other required string',
+            ]
+        ]);
+
+        $this->assertFalse($coercedValue->hasErrors());
+
+        // Test with non-nullable boolean input fields
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $schema = newSchema([
+            'query' => newObjectType([
+                'name'   => 'Query',
+                'fields' => [
+                    'inputObjectField' => [
+                        'type' => booleanType(),
+                        'args' => [
+                            'inputObject' => [
+                                'type' => newInputObjectType([
+                                        'name'   => 'InputObject',
+                                        'fields' => [
+                                            'a' => ['type' => booleanType()],
+                                            'b' => ['type' => newNonNull(booleanType())]
+                                        ]
+                                    ]
+                                )
+                            ],
+                        ],
+                    ],
+                ],
+            ]),
+        ]);
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $documentNode = parse('
+            query ($inputObject: InputObject!) {
+                inputObjectField(inputObject: $inputObject)
+            }
+         ');
+
+        /** @var OperationDefinitionNode $operation */
+        $operation           = $documentNode->getDefinitions()[0];
+        $variableDefinitions = $operation->getVariableDefinitions();
+
+        // Test with a missing non-null string
+        $coercedValue = coerceVariableValues($schema, $variableDefinitions, [
+            'inputObject' => [
+                'a' => true
+            ]
+        ]);
+
+        $this->assertTrue($coercedValue->hasErrors());
+        $this->assertEquals('Variable "$inputObject" got invalid value {"a":true}; Field value.b of required type Boolean! was not provided.',
+            $coercedValue->getErrors()[0]->getMessage());
+
+        // Test again with all fields present, all booleans true
+        $coercedValue = coerceVariableValues($schema, $variableDefinitions, [
+            'inputObject' => [
+                'a' => true,
+                'b' => true,
+            ]
+        ]);
+
+        $this->assertFalse($coercedValue->hasErrors());
+
+        // Test again with all fields present, all booleans false (this has been problematic before)
+        $coercedValue = coerceVariableValues($schema, $variableDefinitions, [
+            'inputObject' => [
+                'a' => false,
+                'b' => false,
+            ]
+        ]);
+
+        $this->assertFalse($coercedValue->hasErrors());
     }
 }
